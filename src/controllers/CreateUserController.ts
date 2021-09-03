8397e250ed7f00f7f9813b370e652a1ae9e5a54c
martex84 <martex849@gmail.com>
2021-09-03 15:39:12 -0300

    Inclusao Controller CreateUser

---
+++ src/controllers/CreateUserController.ts
@@ -0,0 +1,22 @@
+import { Request, Response } from "express";
+import { CreateUserServices } from "../services/CreateUserServices"
+
+export class CreateUserController {
+    async handle(request: Request, response: Response) {
+        const {
+            name,
+            email,
+            admin
+        } = request.body;
+
+        const createUserService = new CreateUserServices();
+
+        const user = await createUserService.execute({
+            name,
+            email,
+            admin
+        });
+
+        return response.json(user);
+    }
+}
